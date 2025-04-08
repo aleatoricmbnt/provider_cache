d82c3b5d152a55219ba2f67da2e41d945d68d299
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-04-08 14:00:23 +0300

    Same provider, different version (via modules) // private registry

---
+++ no_lockfile/multiple_providers_private_registry/scalr_prod_module_3_0_0/agent_pool.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    scalr = {
+      source  = "registry.scalr.io/scalr/scalr"
+      version = "3.0.0"
+    }
+  }
+}
+
+data "scalr_current_account" "account" {}
+
+resource "scalr_agent_pool" "default" {
+  name       = "default-pool"
+  account_id = data.scalr_current_account.id
+}
