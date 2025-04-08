b98f431455dc05a5c55cd677aa7369544bc0ef6e
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-04-08 14:29:51 +0300

    Same provider, different version (via modules) // private registry // fix provider names

--- no_lockfile/multi_provider_private_registry_modules/main.tf
+++ no_lockfile/multi_provider_private_registry_modules/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
-    scalr_prod_2 = {
+    scalr-prod-2 = {
       source  = "registry.scalr.io/scalr/scalr"
       version = "2.5.0"
     }
-    scalr_prod_3 = {
+    scalr-prod-3 = {
       source  = "registry.scalr.io/scalr/scalr"
       version = "3.0.0"
     }
-    scalr_stg_master = {
+    scalr-stg-master = {
       source  = "registry.main.scalr.dev/scalr/scalr"
       version = "1.0.0-rc-master"
     }
@@ -19,20 +19,20 @@ terraform {
 module "scalr_prod_module_2_5_0" {
   source = "./scalr_prod_module_2_5_0"
   providers = {
-    scalr = scalr_prod_2
+    scalr = scalr-prod-2
   }
 }
 
 module "scalr_prod_module_3_0_0" {
   source = "./scalr_prod_module_3_0_0"
   providers = {
-    scalr = scalr_prod_3
+    scalr = scalr-prod-3
   }
 }
 
 module "scalr_stg_module_master" {
   source = "./scalr_stg_module_master"
   providers = {
-    scalr = scalr_stg_master
+    scalr = scalr-stg-master
   }
 }
