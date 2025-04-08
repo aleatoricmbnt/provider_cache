8a20689725078d0182b6d8a3b73c37cf2ab3bafd
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-04-08 14:49:10 +0300

    test comment/uncomment 1

--- no_lockfile/multi_provider_private_registry_modules/main.tf
+++ no_lockfile/multi_provider_private_registry_modules/main.tf
@@ -8,11 +8,11 @@ terraform {
     #   source  = "registry.scalr.io/scalr/scalr"
     #   version = "3.0.0"
     # }
-    scalr-stg-master = {
-      source  = "registry.main.scalr.dev/scalr/scalr"
-      version = "1.0.0-rc-master"
-    }
-  }
+  #   scalr-stg-master = {
+  #     source  = "registry.main.scalr.dev/scalr/scalr"
+  #     version = "1.0.0-rc-master"
+  #   }
+  # }
 }
 
 
@@ -30,9 +30,9 @@ module "scalr_prod_module_2_5_0" {
 #   }
 # }
 
-module "scalr_stg_module_master" {
-  source = "./scalr_stg_module_master"
-  providers = {
-    scalr = scalr-stg-master
-  }
-}
+# module "scalr_stg_module_master" {
+#   source = "./scalr_stg_module_master"
+#   providers = {
+#     scalr = scalr-stg-master
+#   }
+# }
