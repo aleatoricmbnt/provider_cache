aaa4904e4a644f1a6a46e253925e9427996aa10c
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-04-09 16:56:41 +0300

    merged separate providers.tf into single file to aboid duplicate terraform {} declaration

--- lockfile/partially_locked_terraform/diff_locked/workspace_b/providers.tf
+++ lockfile/partially_locked_terraform/diff_locked/workspace_b/providers.tf
@@ -1,5 +1,17 @@
 terraform {
   required_providers {
+    bluecat = {
+      source = "umich-vci/bluecat"
+      version = "0.5.0"
+    }
+    bosk = {
+      source = "prdoyle/bosk"
+      version = "0.0.16"
+    }
+    bytebase = {
+      source = "bytebase/bytebase"
+      version = "1.0.21"
+    }
     http = {
       source = "hashicorp/http"
       version = "3.4.4"
--- lockfile/partially_locked_terraform/diff_locked/workspace_c/providers.tf
+++ lockfile/partially_locked_terraform/diff_locked/workspace_c/providers.tf
@@ -1,5 +1,17 @@
 terraform {
   required_providers {
+    logtail = {
+      source = "BetterStackHQ/logtail"
+      version = "0.4.2"
+    }
+    mondoo = {
+      source = "mondoohq/mondoo"
+      version = "0.23.4"
+    }
+    mongodbatlas = {
+      source = "mongodb/mongodbatlas"
+      version = "1.31.0"
+    }
     http = {
       source = "hashicorp/http"
       version = "3.4.4"
