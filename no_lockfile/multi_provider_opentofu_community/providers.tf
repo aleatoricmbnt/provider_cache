e3c8d2ccda51a25b131d5de7263d9482f0eed16a
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-04-09 14:29:44 +0300

    +2 plans // no lockfile // terraform + opentofu

---
+++ no_lockfile/multi_provider_opentofu_community/providers.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    ably = {
+      source = "ably/ably"
+      version = "0.9.0"
+    }
+    aiven = {
+      source = "aiven/aiven"
+      version = "4.37.0"
+    }
+    aptible = {
+      source = "aptible/aptible"
+      version = "0.9.12"
+    }
+    aquasec = {
+      source = "aquasecurity/aquasec"
+      version = "0.8.37"
+    }
+    oasis = {
+      source = "arangodb-managed/oasis"
+      version = "2.3.1"
+    }
+    atlassian-operations = {
+      source = "atlassian/atlassian-operations"
+      version = "1.0.1"
+    }
+    auth0 = {
+      source = "auth0/auth0"
+      version = "1.16.0"
+    }
+    harbor = {
+      source = "bestseller/harbor"
+      version = "3.10.20"
+    }
+    passwordsafe = {
+      source = "beyondtrust/passwordsafe"
+      version = "1.0.3"
+    }
+    hrui = {
+      source = "brennoo/hrui"
+      version = "0.1.0-alpha.10"
+    }
+  }
+}
---
+++ no_lockfile/multi_providers_terraform_other/provider.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    netris = {
+      source = "netrisai/netris"
+      version = "3.6.5"
+    }
+    ns1 = {
+      source = "ns1-terraform/ns1"
+      version = "2.6.1"
+    }
+    nutanix = {
+      source = "nutanix/nutanix"
+      version = "2.1.0"
+    }
+    netapp-cloudmanager = {
+      source = "NetApp/netapp-cloudmanager"
+      version = "24.11.2"
+    }
+    netlify = {
+      source = "netlify/netlify"
+      version = "0.2.2"
+    }
+    nsxt = {
+      source = "vmware/nsxt"
+      version = "3.8.0"
+    }
+    okta = {
+      source = "okta/okta"
+      version = "4.14.0"
+    }
+    oktaasa = {
+      source = "oktadeveloper/oktaasa"
+      version = "1.0.1"
+    }
+    onepassword = {
+      source = "1Password/onepassword"
+      version = "2.1.2"
+    }
+    opal = {
+      source = "opalsecurity/opal"
+      version = "3.0.14"
+    }
+  }
+}
