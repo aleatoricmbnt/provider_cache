51c39927faa17d99da3aaaa306f7a0561b9b9d49
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-04-10 18:06:02 +0300

    Added plan with for 0.12 tf

---
+++ no_lockfile/multiple_providers_terraform _0_12/main.tf
@@ -0,0 +1,41 @@
+# main.tf
+
+terraform {
+  required_version = "0.12.29"
+
+  required_providers {
+    random = "~> 2.2"
+    null   = "~> 2.1"
+    local  = "~> 1.4"
+    template = "~> 2.1"
+  }
+}
+
+provider "random" {}
+provider "null" {}
+provider "local" {}
+provider "template" {}
+
+resource "random_pet" "project_name" {
+  length    = 2
+  separator = "-"
+}
+
+resource "template_file" "greeting" {
+  template = "Hello, ${name}!"
+
+  vars = {
+    name = random_pet.project_name.id
+  }
+}
+
+resource "null_resource" "show_greeting" {
+  provisioner "local-exec" {
+    command = "echo '${template_file.greeting.rendered}'"
+  }
+}
+
+resource "local_file" "output" {
+  content  = template_file.greeting.rendered
+  filename = "${path.module}/output.txt"
+}
