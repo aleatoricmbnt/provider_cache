63c771506deed8dbce74c03f5dccca850ff51080
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-05-01 13:04:00 +0300

    commented external data source (temp)

--- lockfile/all_locked_strict_versions_tofu/no_auth_data.tf
+++ lockfile/all_locked_strict_versions_tofu/no_auth_data.tf
@@ -10,15 +10,15 @@ output "data_out" {
 
 ### EXTERNAL
 
-data "external" "example" {
-  program = ["bash", "${path.module}/example-data-source.sh"]
+# data "external" "example" {
+#   program = ["bash", "${path.module}/example-data-source.sh"]
 
-  query = {
-    # arbitrary map from strings to strings, passed
-    # to the external program as the data query.
-    foo = "abc123"
-    baz = "def456"
-  }
-}
+#   query = {
+#     # arbitrary map from strings to strings, passed
+#     # to the external program as the data query.
+#     foo = "abc123"
+#     baz = "def456"
+#   }
+# }
 
 
